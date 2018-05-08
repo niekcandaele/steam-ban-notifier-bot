b5efdc226aad48685bf9afdb1958df102184308f
niekcandaele <niekcandaele@gmail.com>
2018-05-08 10:47:23 +0200

    Discord guild model

---
+++ api/models/DiscordGuild.js
@@ -0,0 +1,46 @@
+/**
+ * DiscordGuild.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    attributes: {
+  
+      //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+      //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+      //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+  
+      discordId: {
+        type: 'string',
+        unique: true,
+        required: true
+      },
+
+      banNotificationChannel: {
+          defaultsTo: 0,
+          type: 'string'
+      },
+  
+  
+      //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+      //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+      //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+  
+  
+      //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+      //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+      //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  
+      trackedAccounts: {
+        collection: 'trackedaccount',
+        via: 'trackedByGuild'
+      },
+  
+    },
+  
+  };
+  
+  
