5134948d66cb6ae02cc0e3f9b4e66f7d451bb07a
niekcandaele <niekcandaele@gmail.com>
2018-05-25 16:43:38 +0200

    update with frontend stuff

--- config/routes.js
+++ config/routes.js
@@ -11,6 +11,23 @@
 module.exports.routes = {
 
 
+  //  ╦ ╦╔═╗╔╗ ╔═╗╔═╗╔═╗╔═╗╔═╗
+  //  ║║║║╣ ╠╩╗╠═╝╠═╣║ ╦║╣ ╚═╗
+  //  ╚╩╝╚═╝╚═╝╩  ╩ ╩╚═╝╚═╝╚═╝
+
+  /***************************************************************************
+  *                                                                          *
+  * Make the view located at `views/homepage.ejs` your home page.            *
+  *                                                                          *
+  * (Alternatively, remove this and add an `index.html` file in your         *
+  * `assets` directory)                                                      *
+  *                                                                          *
+  ***************************************************************************/
+
+  '/': {
+    view: 'pages/homepage'
+  },
+
   /***************************************************************************
   *                                                                          *
   * More custom routes here...                                               *
