8defe0dced4e928393a46ca1e79579de9c313ff6
niekcandaele <niekcandaele@gmail.com>
2018-05-26 11:45:31 +0200

    Add api endpoint to check if we can open a dm channel

---
+++ api/controllers/discord-bot/check-open-dm.js
@@ -0,0 +1,57 @@
+module.exports = {
+
+
+  friendlyName: 'Check open dm',
+
+
+  description: 'Check if the discord bot can open a DM channel with the logged in user',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    let user = await User.findOne(this.req.session.userId);
+
+    if (_.isUndefined(user)) {
+      return exits.error(new Error(`Not a valid user logged in`))
+    }
+
+    if (!user.discordId) {
+      return exits.success(false);
+    }
+
+    let discordClient = sails.hooks.discordbot.getClient();
+    let discordUser = discordClient.users.get(user.discordId);
+    
+    if (!discordUser) {
+      return exits.success(false);
+    }
+
+    try {
+      let dmChannel = await discordUser.openDM();
+
+      sails.log.debug(`Checking if we can send DM to discord user ${user.discordId} ${dmChannel ? true : false}`)
+
+      if (!dmChannel) {
+        return exits.success(false);
+      } else {
+        return exits.success(true)
+      }
+
+    } catch (error) {
+      return exits.success(false);
+    }
+
+  }
+
+
+};
