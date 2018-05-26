ffc5f0d5b1fcd3aec76dc0c30d8f49642f20642d
niekcandaele <niekcandaele@gmail.com>
2018-05-26 11:49:19 +0200

    Implement test DM action & route

---
+++ api/controllers/discord-bot/test-dm.js
@@ -0,0 +1,50 @@
+module.exports = {
+
+
+  friendlyName: 'Test dm',
+
+
+  description: 'Send a test DM to a logged in user',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+
+    let user = await User.findOne(this.req.session.userId);
+
+    if (_.isUndefined(user)) {
+      return exits.error(new Error(`Not a valid user logged in`))
+    }
+
+    if (!user.discordId) {
+      return exits.success(false);
+    }
+
+    let discordClient = sails.hooks.discordbot.getClient();
+    let discordUser = discordClient.users.get(user.discordId);
+    
+    if (!discordUser) {
+      return exits.success(false);
+    }
+
+    try {
+      let dmChannel = await discordUser.openDM();
+      await dmChannel.send(`This is a test message! If you received this, means everything is working as it should! If you didn't receive this, well you better stop reading.. `)
+      return exits.success(true);
+    } catch (error) {
+      sails.log.warn(error);
+      return exits.success(false);
+    }
+
+  }
+};
