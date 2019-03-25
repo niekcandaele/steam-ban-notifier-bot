e307fba0d02e837d49482799179acd9ab97ddffe
Niek Candaele <niekcandaele@gmail.com>
2019-03-25 15:43:53 +0100

    Fix another error when user is not logged in

--- api/controllers/discord-bot/check-open-dm.js
+++ api/controllers/discord-bot/check-open-dm.js
@@ -13,11 +13,18 @@ module.exports = {
 
 
   exits: {
-
+    badRequest: {
+      responseType: 'badRequest',
+      statusCode: 400
+    }
   },
 
 
   fn: async function (inputs, exits) {
+
+    if (!this.req.session.userId) {
+      return exits.badRequest('You must be logged in');
+    }
 
     let user = await User.findOne(this.req.session.userId);
 
@@ -31,7 +38,7 @@ module.exports = {
 
     let discordClient = sails.hooks.discordbot.getClient();
     let discordUser = discordClient.users.get(user.discordId);
-    
+
     if (!discordUser) {
       return exits.success(false);
     }
--- api/controllers/steamBot/check-if-friend.js
+++ api/controllers/steamBot/check-if-friend.js
@@ -1,37 +1,43 @@
 module.exports = {
 
 
-    friendlyName: 'Check if friend',
-  
-  
-    description: '',
-  
-  
-    inputs: {
-  
-    },
-  
-  
-    exits: {
-  
-    },
-  
-  
-    fn: async function (inputs, exits) {
-  
-      let user = await User.findOne(this.req.session.userId);
-  
-      if (_.isUndefined(user)) {
-        return exits.error(new Error(`Did not find a valid user.`))
-      }
+  friendlyName: 'Check if friend',
 
-      let friendStatus = await sails.hooks.steambot.getFriendStatus(user.steamId);
-  
-      sails.log.debug(`Checked if steam friends with ${user.steamId} - ${friendStatus}`)
-      return exits.success(friendStatus);
-  
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+    badRequest: {
+      responseType: 'badRequest',
+      statusCode: 400
     }
-  
-  
-  };
-  
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    if (!this.req.session.userId) {
+      return exits.badRequest('You must be logged in');
+    }
+
+    let user = await User.findOne(this.req.session.userId);
+
+    if (_.isUndefined(user)) {
+      return exits.error(new Error(`Did not find a valid user.`))
+    }
+
+    let friendStatus = await sails.hooks.steambot.getFriendStatus(user.steamId);
+
+    sails.log.debug(`Checked if steam friends with ${user.steamId} - ${friendStatus}`)
+    return exits.success(friendStatus);
+
+  }
+
+
+};
