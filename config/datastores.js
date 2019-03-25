5c1356d6196e2147ee0e1f08009b373c8961b0f4
Niek Candaele <niekcandaele@gmail.com>
2019-03-25 12:57:33 +0100

    Update dependencies

--- config/datastores.js
+++ config/datastores.js
@@ -48,7 +48,7 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    adapter: 'sails-disk',
+    adapter: 'sails-mysql',
     url: process.env.DBSTRING,
 
   },
