819195c9c7bfa8bf89f965ed761ac5a2d9d5ee3f
Niek Candaele <niekcandaele@gmail.com>
2019-03-25 15:41:36 +0100

    Fix a bug when user is not logged in

--- api/controllers/discord-bot/check-mutual-guild.js
+++ api/controllers/discord-bot/check-mutual-guild.js
@@ -13,11 +13,18 @@ module.exports = {
 
 
   exits: {
-
+    badRequest: {
+      responseType: 'badRequest',
+      statusCode: 400
+}
   },
 
 
   fn: async function (inputs, exits) {
+
+    if (!this.req.session.userId) {
+      return exits.badRequest('You must be logged in');
+    }
 
     let user = await User.findOne(this.req.session.userId);
     
